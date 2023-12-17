186cc3fc4c4f898a4b2333ef39d52180bdd37758
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-17 16:15:14 +0500

    Add Dot Navigation

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/device/device_utility.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
+import 'widgets/onboarding_dot_navigation.dart';
 import 'widgets/onboarding_page.dart';
 import 'widgets/onboarding_skip.dart';
 
@@ -11,36 +17,38 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        // Horizontal Scrollable Pages
-        PageView(
-          children: const [
-            OnBoardingPage(
-              image: TImages.onBoardingImage1,
-              title: TTexts.onBoardingTitle1,
-              subTitle: TTexts.onBoardingSubTitle1,
-            ),
-            OnBoardingPage(
-              image: TImages.onBoardingImage2,
-              title: TTexts.onBoardingTitle2,
-              subTitle: TTexts.onBoardingSubTitle2,
-            ),
-            OnBoardingPage(
-              image: TImages.onBoardingImage3,
-              title: TTexts.onBoardingTitle3,
-              subTitle: TTexts.onBoardingSubTitle3,
-            ),
-          ],
-        ),
+      body: Stack(
+        children: [
+          // Horizontal Scrollable Pages
+          PageView(
+            children: const [
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+              ),
+            ],
+          ),
 
-        // Skip Button
-        const OnBoardingSkip()
+          // Skip Button
+          const OnBoardingSkip(),
 
-        // Dot Navigation SmoothPageIndicator
+          // Dot Navigation SmoothPageIndicator
+          const OnBoardingNavigation(),
 
-        // Circular Button
-      ],
-    ));
+          // Circular Button
+        ],
+      ),
+    );
   }
 }
