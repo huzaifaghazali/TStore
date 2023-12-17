342ebda95c149db68d9a9e0d4486e48529619761
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-17 15:42:09 +0500

    Add Horizontal Scrollable Pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/utils/theme/theme.dart';
+import 'package:t_store/app.dart';
 
 void main() {
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo:  Initialize Authentication
+
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
