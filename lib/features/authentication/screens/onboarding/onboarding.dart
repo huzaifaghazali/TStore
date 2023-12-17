f6e40f731e8b5331ac335af50fde326fd0d3c91f
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-17 15:58:36 +0500

    Add Skip button and refactor code

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
-import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
-import 'package:t_store/utils/helpers/helper_functions.dart';
+
+import 'widgets/onboarding_page.dart';
+import 'widgets/onboarding_skip.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -31,52 +32,15 @@ class OnBoardingScreen extends StatelessWidget {
               subTitle: TTexts.onBoardingSubTitle3,
             ),
           ],
-        )
+        ),
 
         // Skip Button
+        const OnBoardingSkip()
 
         // Dot Navigation SmoothPageIndicator
 
         // Circular Button
       ],
     ));
-  }
-}
-
-class OnBoardingPage extends StatelessWidget {
-  const OnBoardingPage({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.subTitle,
-  });
-
-  final String image, title, subTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(TSizes.defaultSpace),
-      child: Column(
-        children: [
-          Image(
-            width: THelperFunctions.screenWidth() * 0.8,
-            height: THelperFunctions.screenHeight() * 0.6,
-            image: AssetImage(image),
-          ),
-          Text(
-            title,
-            style: Theme.of(context).textTheme.headlineMedium,
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: TSizes.spaceBtwItems),
-          Text(
-            subTitle,
-            style: Theme.of(context).textTheme.bodyMedium,
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
-    );
   }
 }
