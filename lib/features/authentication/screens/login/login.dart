8591cbe183b9c35074a7ee49994e0c8a9c121fe5
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-18 14:42:33 +0500

    Add Login Form

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/styles/spacing_style.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
@@ -33,7 +34,54 @@ class LoginScreen extends StatelessWidget {
                   Text(TTexts.loginSubTitle,
                       style: Theme.of(context).textTheme.bodyMedium),
                 ],
-              )
+              ),
+
+              // Form
+              Form(
+                child: Column(
+                  children: [
+                    // Email
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.direct_right),
+                        labelText: TTexts.email,
+                      ),
+                    ),
+
+                    const SizedBox(height: TSizes.spaceBtwInputFields),
+
+                    // Password
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.password_check),
+                        labelText: TTexts.password,
+                        suffixIcon: Icon(Iconsax.eye_slash),
+                      ),
+                    ),
+
+                    const SizedBox(height: TSizes.spaceBtwInputFields / 2),
+
+                    // Remember me  Forget Password
+                    Row(
+                      children: [
+                        // Remember Me
+                        Row(
+                          children: [
+                            Checkbox(value: true, onChanged: (value) {}),
+                            const Text(TTexts.rememberMe),
+                          ],
+                        ),
+
+                        // Forget Password
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(TTexts.forgetPassword),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
