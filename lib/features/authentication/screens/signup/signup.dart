0cb10fc6c0e28337afa2179fb1c44c7fa46f701c
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-19 14:48:37 +0500

    Refactor code

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_utils/get_utils.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets.login_signup/form_divider.dart';
 import 'package:t_store/common/widgets.login_signup/social_buttons.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
-import 'package:t_store/utils/helpers/helper_functions.dart';
 
-import '../../../../utils/constants/colors.dart';
+import 'widgets/signupform.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = THelperFunctions.isDarkMode(context);
+
     return Scaffold(
       /// Appbar
         appBar: AppBar(),
@@ -29,92 +27,9 @@ class SignupScreen extends StatelessWidget {
                     const SizedBox(height: TSizes.spaceBtwSections),
 
                     /// Form
-                    Form(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: TextFormField(
-                                  expands: false,
-                                  decoration: const InputDecoration(labelText: TTexts.firstName, prefixIcon: Icon(Iconsax.user)),
-                                ),
-                              ),
-                              const SizedBox(width: TSizes.spaceBtwInputFields),
-                              Expanded(
-                                child: TextFormField(
-                                  expands: false,
-                                  decoration: const InputDecoration(labelText: TTexts.lastName, prefixIcon: Icon(Iconsax.user)),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: TSizes.spaceBtwInputFields),
-
-                          /// Username
-                          TextFormField(
-                            expands: false,
-                            decoration: const InputDecoration(labelText: TTexts.username, prefixIcon: Icon(Iconsax.user_edit)),
-                          ),
-                          const SizedBox(height: TSizes.spaceBtwInputFields),
-
-                          /// Email
-                          TextFormField(
-                            decoration: const InputDecoration(labelText: TTexts.email, prefixIcon: Icon(Iconsax.direct)),
-                          ),
-                          const SizedBox(height: TSizes.spaceBtwInputFields),
-
-                          /// Phone Number
-                          TextFormField(
-                            decoration: const InputDecoration(labelText: TTexts.phoneNo, prefixIcon: Icon(Iconsax.call)),
-                          ),
-                          const SizedBox(height: TSizes.spaceBtwInputFields),
-
-                          /// Password
-                          TextFormField(
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              labelText: TTexts.password,
-                              prefixIcon: Icon(Iconsax.call),
-                              suffixIcon: Icon(Iconsax.eye_slash),
-                            ),
-                          ),
-                          const SizedBox(height: TSizes.spaceBtwInputFields),
-
-                          /// Terms & Condition checkbox
-                          Row(
-                            children: [
-                              SizedBox(width: 24, height: 24, child: Checkbox(value: true, onChanged: (value){})),
-                              const SizedBox(width: TSizes.spaceBtwItems),
-                              Expanded(
-                                child: Text.rich(
-                                  TextSpan(
-                                      children: [
-                                        TextSpan(text: '${TTexts.iAgreeTo} ', style: Theme.of(context).textTheme.bodySmall),
-                                        TextSpan(text: '${TTexts.privacyPolicy} ', style: Theme.of(context).textTheme.bodyMedium!.apply(
-                                            color: dark ? TColors.white : TColors.primary,
-                                            decoration: TextDecoration.underline,
-                                            decorationColor: dark ? TColors.white: TColors.primary
-                                        )),
-                                        TextSpan(text: '${TTexts.and} ', style: Theme.of(context).textTheme.bodySmall),
-                                        TextSpan(text: TTexts.termsOfUse, style: Theme.of(context).textTheme.bodyMedium!.apply(
-                                            color: dark ? TColors.white : TColors.primary,
-                                            decoration: TextDecoration.underline,
-                                            decorationColor: dark ? TColors.white: TColors.primary
-                                        )),
-                                      ]
-                                  ),),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: TSizes.spaceBtwSections),
-
-                          ///  Sign up Button
-                          SizedBox(width: double.infinity, child: ElevatedButton(onPressed: (){}, child: const Text(TTexts.createAccount),),)
-                        ],
-                      ),
-                    ),
+                    const TSignupForm(),
                     const SizedBox(height: TSizes.spaceBtwSections),
+
                     /// Divider
                     TFormDivider(dividerText: TTexts.orSignInWith.capitalize!),
                     const SizedBox(height: TSizes.spaceBtwSections),
