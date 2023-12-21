29467fc2e21519accb6c5cd82aec1c3f53a975ca
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-21 13:11:06 +0500

    Add Forget Password screen

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:t_store/features/authentication/screens/signup/signup.dart';
 
 import '../../../../../utils/constants/sizes.dart';
@@ -55,7 +56,7 @@ class TLoginForm extends StatelessWidget {
 
                 /// Forget Password
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () => Get.to (() => const ForgetPassword()),
                   child: const Text(TTexts.forgetPassword),
                 ),
               ],
