323e1d30260077dd178ac2697d1bbe367800f0d8
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-22 19:50:13 +0500

    Add curved edges

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/curved_shapes/curved_edges.dart';
 import 'package:t_store/utils/constants/colors.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
@@ -12,16 +13,19 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              color: TColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: SizedBox(
-                height: 400,
-                child: Stack(
-                  children: [
-                    Positioned(top: -150, right: -250, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
-                    Positioned(top: 100, right: -300, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
-                  ],
+            ClipPath(
+              clipper: TCustomCurvedEdges(),
+              child: Container(
+                color: TColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(top: -150, right: -250, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
+                      Positioned(top: 100, right: -300, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
+                    ],
+                  ),
                 ),
               ),
             )
