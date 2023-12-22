990a4018e5ac783c7947e36f610cd0a1f1097c8d
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-22 19:06:18 +0500

    Add Circular container in home screen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Home Screen')
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: TColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  Container(
+                    width: 400,
+                    height: 400,
+                    padding: const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(400),
+                      color: TColors.textWhite.withOpacity(0.1),
+                    ),
+                  ),
+                  Container(
+                    width: 400,
+                    height: 400,
+                    padding: const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(400),
+                      color: TColors.textWhite.withOpacity(0.1),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        )
+      )
     );
   }
 }
