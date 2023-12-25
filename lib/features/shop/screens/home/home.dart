629ff443ce7c8d8d09a5a348380119235e2d9bc3
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-25 14:22:46 +0500

    Add Carousel slider

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
@@ -58,7 +59,17 @@ class HomeScreen extends StatelessWidget {
             /// Body
             Padding(
               padding: const EdgeInsets.all(TSizes.defaultSpace),
-              child: const TRoundedImage(imageUrl: TImages.banner4),
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  viewportFraction: 1,
+
+                ),
+                items: const [
+                  TRoundedImage(imageUrl: TImages.promoBanner1),
+                  TRoundedImage(imageUrl: TImages.promoBanner2),
+                  TRoundedImage(imageUrl: TImages.promoBanner3),
+                ],
+              ),
             ),
           ],
         ),
