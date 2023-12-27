2e2fe2c1c741cc5be8e1577689ca5d1f1a387838
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-27 14:37:37 +0500

    Add image icon in store screen

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:t_store/common/widgets/products/cart/cart_menu_icon.dart';
 import 'package:t_store/common/widgets/texts/section_heading.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 
@@ -41,9 +43,34 @@ class StoreScreen extends StatelessWidget {
                   const TSearchContainer(text: 'Search in Store', showBorder: true, showBackground: false, padding: EdgeInsets.zero),
                   const SizedBox(height: TSizes.spaceBtwItems),
 
-                  /// Featured Brands
+                  /// Heading
                   TSectionHeading(title: 'Featured Brands', onPressed: (){}),
                   const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+                  ///  Featured Brands
+                  TRoundedContainer(
+                    padding: const EdgeInsets.all(TSizes.sm),
+                    showBorder: true,
+                    backgroundColor: Colors.transparent,
+                    child: Row(
+                      children: [
+                        /// -- Icon
+                        Container(
+                          width: 56,
+                          height: 56,
+                          padding: const EdgeInsets.all(TSizes.sm),
+                          decoration: BoxDecoration(
+                            color: dark ? TColors.black : TColors.white,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Image(
+                            image: const AssetImage(TImages.clothIcon),
+                            color: dark ? TColors.white : TColors.dark,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
 
                 ],
               ),
