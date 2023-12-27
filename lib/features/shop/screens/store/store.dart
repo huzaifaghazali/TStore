5e4810f7d4ad2ca88561d5def939f0a160d99f43
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-27 14:29:49 +0500

    Add Heading in store screen

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:t_store/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:t_store/common/widgets/texts/section_heading.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
@@ -34,12 +35,15 @@ class StoreScreen extends StatelessWidget {
               child: ListView(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
-                children: const [
+                children: [
                   /// Search bar
-                  SizedBox(height: TSizes.spaceBtwItems),
-                  TSearchContainer(text: 'Search in Store', showBorder: true, showBackground: false, padding: EdgeInsets.zero),
-                  SizedBox(height: TSizes.spaceBtwItems),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  const TSearchContainer(text: 'Search in Store', showBorder: true, showBackground: false, padding: EdgeInsets.zero),
+                  const SizedBox(height: TSizes.spaceBtwItems),
 
+                  /// Featured Brands
+                  TSectionHeading(title: 'Featured Brands', onPressed: (){}),
+                  const SizedBox(height: TSizes.spaceBtwItems / 1.5),
 
                 ],
               ),
