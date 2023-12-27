8a0552a99c810c180e0f20d174f8fd8439ec752b
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-27 15:35:10 +0500

    Add Grid view in store screen

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/Layouts/grid_layout.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:t_store/common/widgets/products/cart/cart_menu_icon.dart';
 import 'package:t_store/common/widgets/texts/section_heading.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/enums.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 
 import '../../../../common/widgets/images/t_circular_image.dart';
+import '../../../../common/widgets/texts/t_brand_title_text_with_verfied_icon.dart';
 
 class StoreScreen extends StatelessWidget {
   const StoreScreen({super.key});
@@ -50,30 +53,46 @@ class StoreScreen extends StatelessWidget {
                   const SizedBox(height: TSizes.spaceBtwItems / 1.5),
 
                   ///  Featured Brands
-                  TRoundedContainer(
-                    padding: const EdgeInsets.all(TSizes.sm),
-                    showBorder: true,
-                    backgroundColor: Colors.transparent,
-                    child: Row(
-                      children: [
-                        /// -- Icon
-                        TCircularImage(
-                          isNetworkImage: false,
-                          image: TImages.clothIcon,
-                          backgroundColor: Colors.transparent,
-                          overLayColor: dark ? TColors.white : TColors.black,
-                        ),
-                        const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+                 TGridLayout(itemCount: 4, mainAxisExtent: 80, itemBuilder: (_, index) {
+                   return  GestureDetector(
+                     onTap: (){},
+                     child: TRoundedContainer(
+                       padding: const EdgeInsets.all(TSizes.sm),
+                       showBorder: true,
+                       backgroundColor: Colors.transparent,
+                       child: Row(
+                         children: [
+                           /// -- Icon
+                           Flexible(
+                             child: TCircularImage(
+                               isNetworkImage: false,
+                               image: TImages.clothIcon,
+                               backgroundColor: Colors.transparent,
+                               overLayColor: dark ? TColors.white : TColors.black,
+                             ),
+                             ),
+                           const SizedBox(height: TSizes.spaceBtwItems / 2),
 
-                        /// -- Text
-                        Column(
-                          children: [
-
-                          ],
-                        )
-                      ],
-                    ),
-                  )
+                           /// -- Text
+                           Expanded(
+                             child: Column(
+                               mainAxisSize: MainAxisSize.min,
+                               crossAxisAlignment: CrossAxisAlignment.start,
+                               children: [
+                                 const TBrandTitleWithVerifiedIcon(title: 'Nike', brandTextSize: TextSizes.large),
+                                 Text(
+                                   '256 products',
+                                   overflow: TextOverflow.ellipsis,
+                                   style: Theme.of(context).textTheme.labelMedium,
+                                 )
+                               ],
+                             ),
+                           )
+                         ],
+                       ),
+                     ),
+                   );
+                 })
 
                 ],
               ),
