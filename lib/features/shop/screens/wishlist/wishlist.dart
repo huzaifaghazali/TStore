7a670e21ad5d8f5d4c4a5bc102a404e5ed64efc0
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-29 21:53:42 +0500

    Add Product in favourite screen

--- lib/features/shop/screens/wishlist/wishlist.dart
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/Layouts/grid_layout.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/icons/t_circular_icon.dart';
+import 'package:t_store/common/widgets/products/products_cart/product_cart_vertical.dart';
 import 'package:t_store/features/shop/screens/home/home.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 class FavouriteScreen extends StatelessWidget {
   const FavouriteScreen({super.key});
@@ -16,6 +19,16 @@ class FavouriteScreen extends StatelessWidget {
         actions: [
           TCircularIcon(icon: Iconsax.add, onPressed: () => Get.to(const HomeScreen())),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              TGridLayout(itemCount: 6, itemBuilder: (_, index) => const TProductCartVertical())
+            ],
+          ),
+        ),
       ),
     );
   }
