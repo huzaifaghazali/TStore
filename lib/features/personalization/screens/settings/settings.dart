71f1b97eb0fc279655436464bbc7a303ff895e33
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-30 15:54:42 +0500

    Add User profile card in settings screen

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/common/widgets/images/t_circular_image.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -18,6 +21,14 @@ class SettingsScreen extends StatelessWidget {
               child: Column(
                 children: [
                   TAppBar(title: Text('Account', style: Theme.of(context).textTheme.headlineMedium!.apply(color: TColors.white))),
+
+                  /// User Profile Card
+                  ListTile(
+                    leading: const TCircularImage( image: TImages.user, width: 50, height: 50, padding: 0),
+                    title: Text('Huzaifa Ghazali', style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)),
+                    subtitle: Text('huzaifa@gmail.com', style: Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white)),
+                    trailing: IconButton(onPressed: (){}, icon: const Icon(Iconsax.edit, color: TColors.white)),
+                  ),
                   const SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
