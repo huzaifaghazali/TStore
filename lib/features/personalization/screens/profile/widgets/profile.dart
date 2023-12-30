73e101f88f924073fc7f9d863b4e77c559ce2edf
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-30 17:02:00 +0500

    Add image icon in profile screen

--- lib/features/personalization/screens/profile/widgets/profile.dart
+++ lib/features/personalization/screens/profile/widgets/profile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/images/t_circular_image.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -7,9 +10,25 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: TAppBar(showBackArrow: true, title: Text('Profile')),
+      appBar: const TAppBar(showBackArrow: true, title: Text('Profile')),
       body: SingleChildScrollView(
-
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Profile Picture
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const TCircularImage(image: TImages.user, width: 80, height: 80),
+                    TextButton(onPressed: (){}, child: const Text('Change Profile Picture'))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
