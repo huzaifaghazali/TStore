b3128374aa4505714ed1883695e43b325d3e26ea
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-01 15:42:26 +0500

    Removed unused imports

--- lib/features/shop/screens/product_details/product_details.dart
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:t_store/common/widgets/appbar/appbar.dart';
-import 'package:t_store/common/widgets/custom_shapes/curved_shapes/curved_edges_widget.dart';
-import 'package:t_store/common/widgets/icons/t_circular_icon.dart';
-import 'package:t_store/common/widgets/images/t_rounded_image.dart';
 import 'package:t_store/features/shop/screens/product_details/widgets/product_meta_data.dart';
-import 'package:t_store/utils/constants/colors.dart';
-import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
-import 'package:t_store/utils/helpers/helper_functions.dart';
-
 import 'widgets/product_details_image_slider.dart';
 import 'widgets/rating_share_widget.dart';
 
@@ -18,7 +9,6 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final dark = THelperFunctions.isDarkMode(context);
     return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
