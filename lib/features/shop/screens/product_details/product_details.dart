b752099a523a3aba03253bc94008bde07a3c36e1
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-01 14:48:17 +0500

    Add image slider in product details screen

--- lib/features/shop/screens/product_details/product_details.dart
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/custom_shapes/curved_shapes/curved_edges_widget.dart';
+import 'package:t_store/common/widgets/images/t_rounded_image.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class ProductDetailScreen extends StatelessWidget {
@@ -18,10 +20,41 @@ class ProductDetailScreen extends StatelessWidget {
             TCurvedEdgeWidget(
               child: Container(
                 color: dark ? TColors.darkerGrey : TColors.light,
-                child: const Stack(
+                child: Stack(
                   children: [
                     /// Main Large Image
-                    Image(image: AssetImage(TImages.productImage1))
+                    const SizedBox(
+                      height: 400,
+                      child: Padding(
+                        padding: EdgeInsets.all(TSizes.productImageRadius * 2),
+                        child: Center(child: Image(image: AssetImage(TImages.productImage5))),
+                      ),
+                    ),
+                    /// Image Slider
+                    Positioned(
+                      right: 0,
+                      bottom: 30,
+                      left: TSizes.defaultSpace,
+                      child: SizedBox(
+                        height: 80,
+                        child: ListView.separated(
+                          separatorBuilder: (_, __) => const SizedBox(width: TSizes.spaceBtwItems),
+                          itemCount: 6,
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          itemBuilder: (_, index) => TRoundedImage(
+                            width: 80,
+                            backgroundColor: dark ? TColors.dark : TColors.white,
+                            border: Border.all(color: TColors.primary),
+                            padding: const EdgeInsets.all(TSizes.sm),
+                            imageUrl: TImages.productImage3,
+                          ),
+                        ),
+                      ),
+                    ),
+
+
                   ],
                 ),
               ),
