457c81ecd0378da5646fd37b135d8c9275dcad18
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-01 14:52:03 +0500

    Add App bar in product details screen

--- lib/features/shop/screens/product_details/product_details.dart
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/custom_shapes/curved_shapes/curved_edges_widget.dart';
+import 'package:t_store/common/widgets/icons/t_circular_icon.dart';
 import 'package:t_store/common/widgets/images/t_rounded_image.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
@@ -53,6 +56,14 @@ class ProductDetailScreen extends StatelessWidget {
                         ),
                       ),
                     ),
+
+                    /// AppBar
+                    const TAppBar(
+                      showBackArrow: true,
+                      actions: [
+                        TCircularIcon(icon: Iconsax.heart5,color: Colors.red)
+                      ],
+                    )
 
 
                   ],
