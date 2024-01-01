d595e673691da8c21fd910d75a296161490fbe69
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-01 15:09:42 +0500

    Add Rating and share button in product details

--- lib/features/shop/screens/product_details/product_details.dart
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -17,14 +17,49 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             /// Product Image slider
-            TProductImageSlider(),
+            const TProductImageSlider(),
 
             /// Product Details
+            Padding(
+              padding: const EdgeInsets.only(right: TSizes.defaultSpace, left:  TSizes.defaultSpace, bottom:  TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// Rating & Share Button
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      /// Rating
+                      Row(
+                        children: [
+                          const Icon(Iconsax.star5, color: Colors.amber, size: 24),
+                          const SizedBox(width: TSizes.spaceBtwItems / 2 ),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(text: '5.0', style: Theme.of(context).textTheme.bodyLarge),
+                                const TextSpan(text: '(199)'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      /// Share Button
+                      IconButton(onPressed: (){}, icon: const Icon(Icons.share, size: TSizes.iconMd)),
+                    ],
+                  )
+                  /// Price, Title, Stack & Brand
+                  ///  Attributes
+                  ///  Checkout Button
+                  ///  Description
+                  ///  Reviews
+                ],
+              ),
+            )
           ],
         ),
       ),
