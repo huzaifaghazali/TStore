ade057e38bd4268fb67eb71b8ab54c90c143c3ed
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-03 15:16:22 +0500

    Add User review

--- lib/features/shop/screens/product_reivews/widgets/user_review_card.dart
+++ lib/features/shop/screens/product_reivews/widgets/user_review_card.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:t_store/common/widgets/products/ratings/rating_indicator.dart';
+import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
@@ -34,7 +37,20 @@ class UserReviewCard extends StatelessWidget {
             const SizedBox(width: TSizes.spaceBtwItems),
             Text('01 Nov, 2023', style: Theme.of(context).textTheme.bodyMedium),
           ],
-        )
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        const ReadMoreText(
+          'The user interface of the app is quite intuitive. I was able to navigate and make purchases seamlessly. Great job!',
+          trimLines: 2,
+          trimMode: TrimMode.Line,
+          trimExpandedText: ' show less',
+          trimCollapsedText: ' show more',
+          moreStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: TColors.primary),
+          lessStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: TColors.primary),
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+
+
       ],
     );
   }
