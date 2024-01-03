3a0d5db80a5ce3313a1a9b094ce37add4731f3fc
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-03 14:43:25 +0500

    Refactor progress indicator code

--- lib/features/shop/screens/product_reivews/product_reviews.dart
+++ lib/features/shop/screens/product_reivews/product_reviews.dart
@@ -4,6 +4,8 @@ import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/device/device_utility.dart';
 
+import 'widgets/progress_indicator_and_rating.dart';
+
 class ProductReviewScreen extends StatelessWidget {
   const ProductReviewScreen({super.key});
 
@@ -27,28 +29,15 @@ class ProductReviewScreen extends StatelessWidget {
               Row(
                 children: [
                   Expanded(flex: 3, child: Text('4.8', style: Theme.of(context).textTheme.displayLarge)),
-                  Expanded(
+                  const Expanded(
                     flex: 7,
                     child: Column(
                       children: [
-                        Row(
-                          children: [
-                            Expanded(flex: 1, child: Text('5', style: Theme.of(context).textTheme.bodyMedium)),
-                            Expanded(
-                              flex: 11,
-                              child: SizedBox(
-                                width: TDeviceUtils.getScreenWidth(context) * 0.5,
-                                child: LinearProgressIndicator(
-                                  value: 0.5,
-                                  minHeight: 11,
-                                  backgroundColor: TColors.grey,
-                                  valueColor: const AlwaysStoppedAnimation(TColors.primary),
-                                  borderRadius: BorderRadius.circular(7),
-                                ),
-                              ),
-                            )
-                          ],
-                        )
+                        TRatingProgressIndicator(text: '5', value: 1.0),
+                        TRatingProgressIndicator(text: '4', value: 0.8),
+                        TRatingProgressIndicator(text: '3', value: 0.6),
+                        TRatingProgressIndicator(text: '2', value: 0.4),
+                        TRatingProgressIndicator(text: '1', value: 0.2),
                       ],
                     ),
                   )
