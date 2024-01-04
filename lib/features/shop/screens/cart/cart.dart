f3b6ff599e00650492e7dae4f0d6c3105111ae14
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-04 14:40:48 +0500

    Add brand and  title of product

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/images/t_rounded_image.dart';
+import 'package:t_store/common/widgets/texts/product_title_text.dart';
+import 'package:t_store/common/widgets/texts/t_brand_title_text_with_verfied_icon.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
@@ -34,6 +36,16 @@ class CartScreen extends StatelessWidget {
                       backgroundColor: dark ? TColors.darkerGrey : TColors.light,
                     ),
                     const SizedBox(width: TSizes.spaceBtwItems),
+
+                    /// Title, Price & Size
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TBrandTitleWithVerifiedIcon(title: 'Nike'),
+                        TProductTitleText(title: 'Black Sports shoes', maxLines: 1),
+                      ],
+                    )
+
                   ],
                 )
               ],
