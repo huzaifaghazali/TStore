05f655b204db69372ada44dfe3633fa72094e6ce
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-04 15:03:03 +0500

    Add remove and add buttons

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/icons/t_circular_icon.dart';
 import 'package:t_store/common/widgets/images/t_rounded_image.dart';
 import 'package:t_store/common/widgets/texts/product_title_text.dart';
 import 'package:t_store/common/widgets/texts/t_brand_title_text_with_verfied_icon.dart';
@@ -7,6 +9,8 @@ import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
+
+import '../../../../common/widgets/products/cart/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -25,41 +29,37 @@ class CartScreen extends StatelessWidget {
             separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwSections),
             itemBuilder: (_, index) => Column(
               children: [
+                const TCartItem(),
+                const SizedBox(height: TSizes.spaceBtwItems),
+
                 Row(
                   children: [
-                    /// Image
-                    TRoundedImage(
-                      imageUrl: TImages.productImage1,
-                      width: 60,
-                      height: 60,
-                      padding: const EdgeInsets.all(TSizes.sm),
-                      backgroundColor: dark ? TColors.darkerGrey : TColors.light,
-                    ),
-                    const SizedBox(width: TSizes.spaceBtwItems),
+                    const SizedBox(width: 70),
+                    /// Add Remove Buttons
+                   Row(
+                     children: [
+                       TCircularIcon(
+                         icon: Iconsax.minus,
+                         width: 32,
+                         height: 32,
+                         size: TSizes.md,
+                         color: dark ? TColors.white : TColors.black,
+                         backgroundColor: dark ? TColors.darkerGrey : TColors.light,
+                       ),
+                       const SizedBox(width: TSizes.spaceBtwItems),
+                       Text('2',style: Theme.of(context).textTheme.titleSmall),
+                       const SizedBox(width: TSizes.spaceBtwItems),
+                       const TCircularIcon(
+                         icon: Iconsax.add,
+                         width: 32,
+                         height: 32,
+                         size: TSizes.md,
+                         color: TColors.white,
+                         backgroundColor: TColors.primary,
+                       ),
 
-                    /// Title, Price & Size
-                    Expanded(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const TBrandTitleWithVerifiedIcon(title: 'Nike'),
-                          const TProductTitleText(title: 'Black Sports shoes', maxLines: 1),
-
-                          /// Attributes
-                          Text.rich(
-                            TextSpan(
-                              children: [
-                                TextSpan(text: 'Color: ', style: Theme.of(context).textTheme.bodySmall),
-                                TextSpan(text: 'Green ', style: Theme.of(context).textTheme.bodyLarge),
-                                TextSpan(text: 'Size: ', style: Theme.of(context).textTheme.bodySmall),
-                                TextSpan(text: 'UK 08 ', style: Theme.of(context).textTheme.bodyLarge),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
+                     ],
+                   )
 
                   ],
                 )
@@ -71,3 +71,4 @@ class CartScreen extends StatelessWidget {
     );
   }
 }
+
