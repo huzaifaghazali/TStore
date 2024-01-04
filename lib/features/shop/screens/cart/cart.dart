c7d6ff8700b8f4ebf3931a7e2cb0f98fd8e41595
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-04 15:24:12 +0500

    Add bottom navigation in cart screen

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/texts/product_price_text.dart';
 import 'package:t_store/utils/constants/sizes.dart';
-import 'package:t_store/utils/helpers/helper_functions.dart';
 import '../../../../common/widgets/products/cart/add_remove_button.dart';
 import '../../../../common/widgets/products/cart/cart_item.dart';
 
@@ -13,36 +12,38 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TAppBar(showBackArrow: true, title: Text('Cart', style: Theme.of(context).textTheme.headlineSmall)),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(TSizes.defaultSpace),
-          child: ListView.separated(
-            shrinkWrap: true,
-            itemCount: 4,
-            separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwSections),
-            itemBuilder: (_, index) => const Column(
-              children: [
-                TCartItem(),
-                SizedBox(height: TSizes.spaceBtwItems),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemCount: 10,
+          separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwSections),
+          itemBuilder: (_, index) => const Column(
+            children: [
+              TCartItem(),
+              SizedBox(height: TSizes.spaceBtwItems),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        SizedBox(width: 70),
-                        TProductQuantityWithAddRemoveButton(),
-                      ],
-                    ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 70),
+                      TProductQuantityWithAddRemoveButton(),
+                    ],
+                  ),
 
-                    /// Add Remove Buttons
-                    TProductPriceText(price: '256'),
-                  ],
-                ),
-              ],
-            ),
+                  /// Add Remove Buttons
+                  TProductPriceText(price: '256'),
+                ],
+              ),
+            ],
           ),
         ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ElevatedButton(onPressed: (){}, child: const Text('Checkout \$256.0')),
       ),
     );
   }
