06e5615f1a02929213a1d98630fbbf9aef652da3
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-04 14:37:17 +0500

    Add product images in cart

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/images/t_rounded_image.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
-      appBar: TAppBar(title: Text('Cart', style: Theme.of(context).textTheme.headlineSmall)),
+      appBar: TAppBar(showBackArrow: true, title: Text('Cart', style: Theme.of(context).textTheme.headlineSmall)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: ListView.separated(
+            shrinkWrap: true,
+            itemCount: 4,
+            separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwSections),
+            itemBuilder: (_, index) => Column(
+              children: [
+                Row(
+                  children: [
+                    /// Image
+                    TRoundedImage(
+                      imageUrl: TImages.productImage1,
+                      width: 60,
+                      height: 60,
+                      padding: const EdgeInsets.all(TSizes.sm),
+                      backgroundColor: dark ? TColors.darkerGrey : TColors.light,
+                    ),
+                    const SizedBox(width: TSizes.spaceBtwItems),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
