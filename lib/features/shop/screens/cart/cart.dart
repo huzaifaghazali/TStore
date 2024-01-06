f8d8b07c44f2b67dab4eb9bc274dd9bbb59cf5ca
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-06 14:38:15 +0500

    Refactor cart item

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -4,6 +4,7 @@ import 'package:t_store/common/widgets/texts/product_price_text.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import '../../../../common/widgets/products/cart/add_remove_button.dart';
 import '../../../../common/widgets/products/cart/cart_item.dart';
+import 'widgets/cart_items.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -12,35 +13,13 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TAppBar(showBackArrow: true, title: Text('Cart', style: Theme.of(context).textTheme.headlineSmall)),
-      body: Padding(
-        padding: const EdgeInsets.all(TSizes.defaultSpace),
-        child: ListView.separated(
-          shrinkWrap: true,
-          itemCount: 10,
-          separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwSections),
-          itemBuilder: (_, index) => const Column(
-            children: [
-              TCartItem(),
-              SizedBox(height: TSizes.spaceBtwItems),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(width: 70),
-                      TProductQuantityWithAddRemoveButton(),
-                    ],
-                  ),
-
-                  /// Add Remove Buttons
-                  TProductPriceText(price: '256'),
-                ],
-              ),
-            ],
-          ),
-        ),
+      body: const Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+        /// Items in Cart
+        child: TCartItems(),
       ),
+
+      /// Checkout Button
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(TSizes.defaultSpace),
         child: ElevatedButton(onPressed: (){}, child: const Text('Checkout \$256.0')),
