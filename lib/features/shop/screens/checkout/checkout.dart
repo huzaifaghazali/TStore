c8e96f4e30864662dc93e9bedcd31583368c6bf3
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-06 15:32:02 +0500

    Add billing payment widget in checkout screen

--- lib/features/shop/screens/checkout/checkout.dart
+++ lib/features/shop/screens/checkout/checkout.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:t_store/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:t_store/features/shop/screens/checkout/widgets/billing_payment_section.dart';
+import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
 import '../../../../common/widgets/appbar/appbar.dart';
 import '../../../../common/widgets/products/cart/coupon.widget.dart';
+import '../../../../utils/helpers/helper_functions.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
       appBar: TAppBar(showBackArrow: true, title: Text('Order Review', style: Theme.of(context).textTheme.headlineSmall)),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(TSizes.defaultSpace),
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             children: [
               /// Item in Cart
-              TCartItems(showAddRemoveButtons: false),
-              SizedBox(height: TSizes.spaceBtwItems),
+              const TCartItems(showAddRemoveButtons: false),
+              const SizedBox(height: TSizes.spaceBtwItems),
 
               /// Coupon TextField
-              TCouponCode(),
-              SizedBox(height: TSizes.spaceBtwSections),
+              const TCouponCode(),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Billing Section
+              TRoundedContainer(
+                showBorder: true,
+                padding: const EdgeInsets.all(TSizes.md),
+                backgroundColor: dark ? TColors.black : TColors.white,
+                child: const Column(
+                  children: [
+                    /// Pricing
+                    TBillingPaymentSection(),
+                    const SizedBox(height: TSizes.spaceBtwItems),
+
+                    /// Divider
+                    const Divider(),
+                    const SizedBox(height: TSizes.spaceBtwItems),
+
+                    /// Payment Method
+                    /// Address
+                  ],
+                ),
+              ),
             ],
           ),
         ),
