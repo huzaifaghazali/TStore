763aa575b35f51f9cf2bd15fae23e28c76188281
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-06 15:17:39 +0500

    Refactor checkout screen code

--- lib/features/shop/screens/checkout/checkout.dart
+++ lib/features/shop/screens/checkout/checkout.dart
@@ -1,73 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:t_store/features/shop/screens/cart/widgets/cart_items.dart';
-import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/sizes.dart';
-import 'package:t_store/utils/helpers/helper_functions.dart';
 
 import '../../../../common/widgets/appbar/appbar.dart';
+import '../../../../common/widgets/products/cart/coupon.widget.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
       appBar: TAppBar(showBackArrow: true, title: Text('Order Review', style: Theme.of(context).textTheme.headlineSmall)),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          padding: EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             children: [
               /// Item in Cart
-              const TCartItems(showAddRemoveButtons: false),
-              const SizedBox(height: TSizes.spaceBtwItems),
+              TCartItems(showAddRemoveButtons: false),
+              SizedBox(height: TSizes.spaceBtwItems),
 
               /// Coupon TextField
-              TRoundedContainer(
-                showBorder: true,
-                backgroundColor: dark ? TColors.dark : TColors.white,
-                padding: const EdgeInsets.only(top: TSizes.sm, bottom: TSizes.sm, right: TSizes.sm, left: TSizes.md),
-                child: Row(
-                  children: [
-                    /// TextField
-                    Flexible(
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: 'Have a promo code? Enter here',
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                        ),
-                      ),
-                    ),
-
-                    /// Button
-                    SizedBox(
-                      width: 80,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: dark ? TColors.white.withOpacity(0.5) : TColors.dark.withOpacity(0.5),
-                          backgroundColor: Colors.grey.withOpacity(0.2),
-                          side: BorderSide(color: Colors.grey.withOpacity(0.1)),
-                        ),
-                        child: const Text('Apply'),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: TSizes.spaceBtwSections),
-
+              TCouponCode(),
+              SizedBox(height: TSizes.spaceBtwSections),
             ],
           ),
         ),
       ),
-
     );
   }
 }
+
