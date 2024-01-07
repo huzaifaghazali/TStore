36445bf56aadb8088e76c6a2b3617a75a75be5b3
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-07 14:52:31 +0500

    Add order number and shipping date

--- lib/features/shop/screens/order/widgets/orders_list.dart
+++ lib/features/shop/screens/order/widgets/orders_list.dart
@@ -42,6 +42,55 @@ class TOrderListItems extends StatelessWidget {
               /// 3. Icon
               IconButton(onPressed: (){}, icon: const Icon(Iconsax.arrow_right_34, size: TSizes.iconSm)),
             ],
+          ),
+          const SizedBox(height: TSizes.spaceBtwItems),
+
+          /// Row 2
+          Row(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    /// 1. Icon
+                    const Icon(Iconsax.tag),
+                    const SizedBox(width: TSizes.spaceBtwItems / 2),
+
+                    ///  2. Status & Date
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text( 'Order', style: Theme.of(context).textTheme.labelMedium),
+                          Text('[#256f2]', style: Theme.of(context).textTheme.titleMedium),
+                        ],
+                      ),
+                    ),
+                 ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    /// 1. Icon
+                    const Icon(Iconsax.calendar),
+                    const SizedBox(width: TSizes.spaceBtwItems / 2),
+
+                    ///  2. Status & Date
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text( 'Shipping Date', style: Theme.of(context).textTheme.labelMedium),
+                          Text('03 Feb 2025', style: Theme.of(context).textTheme.titleMedium),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           )
         ],
       ),
