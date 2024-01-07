49cbc3167c9cdf91fe8861adf12d7ec43a74eccf
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-07 15:47:09 +0500

    Add Banner in sub category screen

--- lib/features/shop/screens/sub_category/sub_categories.dart
+++ lib/features/shop/screens/sub_category/sub_categories.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/images/t_rounded_image.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 class SubCategoriesScreen extends StatelessWidget {
   const SubCategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const TAppBar(title: Text('Sports shirts'), showBackArrow: true),
+    return const Scaffold(
+      appBar: TAppBar(title: Text('Sports shirts'), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Banner
+              TRoundedImage( width: double.infinity, imageUrl: TImages.banner3, applyImageRadius: true ),
+              SizedBox(height: TSizes.spaceBtwSections),
 
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
