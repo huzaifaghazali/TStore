e2c60452fe29096cdf0e93d5866613092fed2d49
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-07 14:55:33 +0500

    Add List view in order screen

--- lib/features/shop/screens/order/widgets/orders_list.dart
+++ lib/features/shop/screens/order/widgets/orders_list.dart
@@ -11,88 +11,93 @@ class TOrderListItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context) ;
-    return TRoundedContainer(
-      showBorder: true,
-      padding: const EdgeInsets.all(TSizes.md),
-      backgroundColor: dark ? TColors.dark : TColors.light,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          /// Row 1
-          Row(
-            children: [
-              /// 1. Icon
-              const Icon(Iconsax.ship),
-              const SizedBox(width: TSizes.spaceBtwItems / 2),
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: 5,
+      separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwItems),
+      itemBuilder: (_, __) =>  TRoundedContainer(
+        showBorder: true,
+        padding: const EdgeInsets.all(TSizes.md),
+        backgroundColor: dark ? TColors.dark : TColors.light,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            /// Row 1
+            Row(
+              children: [
+                /// 1. Icon
+                const Icon(Iconsax.ship),
+                const SizedBox(width: TSizes.spaceBtwItems / 2),
 
-              ///  2. Status & Date
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Processing',
-                      style: Theme.of(context).textTheme.bodyLarge!.apply(color: TColors.primary, fontWeightDelta: 1),
-                    ),
-                    Text('07 Nov 2024', style: Theme.of(context).textTheme.headlineSmall),
-                  ],
-                ),
-              ),
-              /// 3. Icon
-              IconButton(onPressed: (){}, icon: const Icon(Iconsax.arrow_right_34, size: TSizes.iconSm)),
-            ],
-          ),
-          const SizedBox(height: TSizes.spaceBtwItems),
-
-          /// Row 2
-          Row(
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    /// 1. Icon
-                    const Icon(Iconsax.tag),
-                    const SizedBox(width: TSizes.spaceBtwItems / 2),
-
-                    ///  2. Status & Date
-                    Expanded(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text( 'Order', style: Theme.of(context).textTheme.labelMedium),
-                          Text('[#256f2]', style: Theme.of(context).textTheme.titleMedium),
-                        ],
+                ///  2. Status & Date
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Processing',
+                        style: Theme.of(context).textTheme.bodyLarge!.apply(color: TColors.primary, fontWeightDelta: 1),
                       ),
-                    ),
-                 ],
+                      Text('07 Nov 2024', style: Theme.of(context).textTheme.headlineSmall),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Row(
-                  children: [
-                    /// 1. Icon
-                    const Icon(Iconsax.calendar),
-                    const SizedBox(width: TSizes.spaceBtwItems / 2),
+                /// 3. Icon
+                IconButton(onPressed: (){}, icon: const Icon(Iconsax.arrow_right_34, size: TSizes.iconSm)),
+              ],
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems),
 
-                    ///  2. Status & Date
-                    Expanded(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text( 'Shipping Date', style: Theme.of(context).textTheme.labelMedium),
-                          Text('03 Feb 2025', style: Theme.of(context).textTheme.titleMedium),
-                        ],
+            /// Row 2
+            Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      /// 1. Icon
+                      const Icon(Iconsax.tag),
+                      const SizedBox(width: TSizes.spaceBtwItems / 2),
+
+                      ///  2. Status & Date
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text( 'Order', style: Theme.of(context).textTheme.labelMedium),
+                            Text('[#256f2]', style: Theme.of(context).textTheme.titleMedium),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                   ],
+                  ),
                 ),
-              ),
-            ],
-          )
-        ],
+                Expanded(
+                  child: Row(
+                    children: [
+                      /// 1. Icon
+                      const Icon(Iconsax.calendar),
+                      const SizedBox(width: TSizes.spaceBtwItems / 2),
+
+                      ///  2. Status & Date
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text( 'Shipping Date', style: Theme.of(context).textTheme.labelMedium),
+                            Text('03 Feb 2025', style: Theme.of(context).textTheme.titleMedium),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
