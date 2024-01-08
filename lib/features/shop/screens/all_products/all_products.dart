38a1f7cbece691eb65f793c31649decb869b92ed
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-08 12:03:05 +0500

    Add Dropdown button in all products screen

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 class AllProducts extends StatelessWidget {
   const AllProducts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TAppBar(title: Text('Popular Products'), showBackArrow: true,),
+    return Scaffold(
+      appBar: const TAppBar(title: Text('Popular Products'), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Dropdown
+              DropdownButtonFormField(
+                decoration: const InputDecoration(prefixIcon: Icon(Iconsax.sort)),
+                onChanged: (value){},
+                items: ['Name', 'Higher Price', 'Lower Price', 'Sale', 'Newest', 'Popularity']
+                    .map((option) => DropdownMenuItem(value: option, child: Text(option)))
+                    .toList(),
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
