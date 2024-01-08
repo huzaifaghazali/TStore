2be49ee277e7be912b884287d9ebda78cb82c389
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-08 12:19:49 +0500

    Add brands in all brands screen

--- lib/features/shop/screens/brand/all_brands.dart
+++ lib/features/shop/screens/brand/all_brands.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/Layouts/grid_layout.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/brands/t_brand_card.dart';
+import 'package:t_store/common/widgets/texts/section_heading.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 class AllBrandsScreen extends StatelessWidget {
   const AllBrandsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TAppBar(title: Text('Brand'), showBackArrow: true),
+    return Scaffold(
+      appBar: const TAppBar(title: Text('Brand'), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Heading
+              const TSectionHeading(title: 'Brands', showActionButton: false),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              /// Brands
+              TGridLayout(itemCount: 10, mainAxisExtent: 80, itemBuilder: (context, index) => const TBrandCard(showBorder: true))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
